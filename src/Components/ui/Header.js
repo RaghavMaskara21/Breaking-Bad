8e8bedb18d4ad911a15ff08b1d6567a97a5a6d21
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-01-28 22:15:17 +0530

    added header and state

---
+++ src/Components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../img/logo.png'
+const Header = () => {
+  return (
+      <header className='center'>
+        <img scr={logo} alt='no img' />
+      </header>
+  )
+}
+
+export default Header;
